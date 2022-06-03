fa8f52453a73ab075b530de4f9cffa21a75edbbb
kspicer80 <50631380+kspicer80@users.noreply.github.com>
2022-06-03 10:19:08 -0500

    first commit

---
+++ lr2_on_real_data.py
@@ -0,0 +1,113 @@
+import pandas as pd
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+pd.options.display.max_rows = 100
+pd.options.display.max_columns = 50
+pd.options.display.float_format = "{:.1f}".format
+
+training_df = pd.read_csv("https://download.mlcc.google.com/mledu-datasets/california_housing_train.csv")
+
+training_df['median_house_value'] /= 1000.0
+
+print(training_df.head())
+
+print(training_df.describe())
+
+def build_model(my_learning_rate):
+    model = tf.keras.models.Sequential()
+    
+    model.add(tf.keras.layers.Dense(units=1, input_shape=(1,)))
+    model.compile(optimizer=tf.keras.optimizers.RMSprop(lr=my_learning_rate), loss='mean_squared_error', metrics=[tf.keras.metrics.RootMeanSquaredError()])
+    
+    return model
+
+def train_model(model, df, feature, label, epochs, batch_size):
+    history = model.fit(x=df[feature],
+                      y=df[label],
+                      batch_size=batch_size,
+                      epochs=epochs)
+
+    trained_weight = model.get_weights()[0]
+    trained_bias = model.get_weights()[1]
+
+    epochs = history.epoch
+  
+    hist = pd.DataFrame(history.history)
+
+    rmse = hist["root_mean_squared_error"]
+
+    return trained_weight, trained_bias, epochs, rmse
+
+def plot_the_model(trained_weight, trained_bias, feature, label):
+    plt.xlabel(feature)
+    plt.ylabel(label)
+
+    random_examples = training_df.sample(n=200)
+    plt.scatter(random_examples[feature], random_examples[label])
+
+    x0 = 0
+    y0 = trained_bias
+    x1 = 10000
+    y1 = trained_bias + (trained_weight * x1)
+    plt.plot([x0, x1], [y0, y1], c='r')
+
+    plt.show()
+
+def plot_the_loss_curve(epochs, rmse):
+  """Plot a curve of loss vs. epoch."""
+
+  plt.figure()
+  plt.xlabel("Epoch")
+  plt.ylabel("Root Mean Squared Error")
+
+  plt.plot(epochs, rmse, label="Loss")
+  plt.legend()
+  plt.ylim([rmse.min()*0.97, rmse.max()])
+  plt.show()  
+
+print("Defined the plot_the_model and plot_the_loss_curve functions.")    
+def plot_the_loss_curve(epochs, rmse):
+    plt.figure()
+    plt.xlabel('Epoch')
+    plt.ylabel('Root Mean Squared Error')
+    
+    plt.plot(epochs, rmse, label='Loss')
+    plt.legend()
+    plt.ylim([rmse.min()*0.97, rmse.max()])
+    plt.show()
+    
+print("Defined plot_the_model and plot_the_loss_curve functions.")
+
+learning_rate = 0.01
+epochs = 30
+batch_size = 30
+
+my_feature = 'total_rooms'
+my_label = 'median_house_value'
+
+my_model = None
+
+my_model = build_model(learning_rate)
+weight, bias, epochs, rmse = train_model(my_model, training_df, my_feature, my_label, epochs, batch_size)
+
+print("\nThe learned weight for your model is %.4f" % weight)
+print("\nThe learned bias for your model is %.4f" % bias)
+
+plot_the_model(weight, bias, my_feature, my_label)
+plot_the_loss_curve(epochs, rmse)
+
+def predict_house_values(n, feature, label):
+    batch = training_df[feature][10000:10000 + n]
+    predicted_values = my_model.predict_on_batch(x=batch)
+    
+    print("feature   label          predicted")
+    print("  value   value          value")
+    print("          in thousand$   in thousand$")
+    print("--------------------------------------")
+    for i in range(n):
+        print ("%5.0f %6.0f %15.0f" % (training_df[feature][10000 + i],
+                                   training_df[label][10000 + i],
+                                   predicted_values[i][0]))
+
+predict_house_values(10, my_feature, my_label)
